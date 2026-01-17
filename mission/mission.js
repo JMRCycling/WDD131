988d4d9c4a1dd6b79f0d813d74fb779044a9ed98
JMRCycling <mtuft1@byui.edu>
2026-01-17 00:31:51 -0700

    additional changes needed

--- mission/mission.js
+++ mission/mission.js
@@ -9,14 +9,16 @@ function changeTheme() {
     if (current == 'dark') {
         // code for changes to colors and logo
         console.log('dark mode');
-        document.body.style.backgroundColor = '#3b3b3b';
+        document.body.style.backgroundColor = '#333';
         document.body.style.color = 'white';
-        document.querySelector('hr').style.backgroundColor = 'white';
+        document.querySelector('h3').style.color = '#5680b6';
+        document.querySelector('hr').style.backgroundColor = '#5680b6';
         logo.setAttribute('src', 'byui-logo-white.png');
     } else {
         console.log('light mode');
         document.body.style.backgroundColor = '#ffffff';
         document.body.style.color = 'black';
+        document.querySelector('h3').style.color = '#035f9c';
         document.querySelector('hr').style.backgroundColor = '#035f9c';
         logo.setAttribute('src', 'https://wddbyui.github.io/wdd131/images/byui-logo-blue.webp');
         // code for changes to colors and logo
