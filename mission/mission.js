abe47d9719fb116dd87be5ca79db22f42a3bd488
JMRCycling <mtuft1@byui.edu>
2026-01-17 14:11:16 -0700

    functional bug fix

--- mission/mission.js
+++ mission/mission.js
@@ -11,14 +11,14 @@ function changeTheme() {
         console.log('dark mode');
         document.body.style.backgroundColor = '#333';
         document.body.style.color = 'white';
-        document.querySelector('h3').style.color = '#5680b6';
+        document.querySelector('h2').style.color = '#5680b6';
         document.querySelector('hr').style.backgroundColor = '#5680b6';
         logo.setAttribute('src', 'byui-logo-white.png');
     } else {
         console.log('light mode');
         document.body.style.backgroundColor = '#ffffff';
         document.body.style.color = 'black';
-        document.querySelector('h3').style.color = '#035f9c';
+        document.querySelector('h2').style.color = '#035f9c';
         document.querySelector('hr').style.backgroundColor = '#035f9c';
         logo.setAttribute('src', 'https://wddbyui.github.io/wdd131/images/byui-logo-blue.webp');
         // code for changes to colors and logo
